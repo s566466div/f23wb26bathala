d0925c1eb147388656bdedb8cff904f77b5869cd
Divya Bathala <S566466@nwmissouri.edu>
2023-11-01 18:44:49 -0500

    updated choose

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fruitsRouter = require('./routes/fruits');
 var boardRouter = require('./routes/board');
-var ChooseRouter = require('./routes/Choose');
+var chooseRouter = require('./routes/choose');
 //var gridbuildRouter = require('./routes/gridbuild');
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/fruits', fruitsRouter);
 app.use('/board', boardRouter);
-app.use('/Choose', ChooseRouter);
+app.use('/choose', chooseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
